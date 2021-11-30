f0306e3234f1ea4d04ae00f1a6aaf39d455cb5bd
Saahil Ali <programmer290399@gmail.com>
2021-11-30 22:14:33 +0530

    wanted to stash but commited instead

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-from python_engine.src import get_transcript_yt, vtt_to_corpus, search_transcript
-from transformers import pipeline
+from python_engine.src import get_answer_with_timestamp
 
-model_name = "deepset/roberta-base-squad2"
-question_answerer = pipeline(
-    model=model_name, tokenizer=model_name, revision="v1.0", task="question-answering"
-)
 
 subs_filename = "subs.vtt"
 
@@ -23,17 +18,11 @@ with col2:
     st.markdown("#")
     if st.button("Load"):
         placeholder.video(url)
-        with open(subs_filename, "w+") as subs:
-            transcript = get_transcript_yt(url)["subtitles"]
-            subs.write(transcript)
-corpus = vtt_to_corpus(subs_filename)
 
 question = st.sidebar.text_input("Whats your question?")
 if st.sidebar.button("Submit"):
-    answer = question_answerer(question=question, context=corpus)["answer"]
-    timestamp = search_transcript(answer, subs_filename)[0]
+    answer, timestamp = get_answer_with_timestamp(url, question)
     placeholder.video(url, start_time=timestamp)
     st.sidebar.write(answer)
-text_answer = st.sidebar.text_area("Your answers will appear below:")
 if st.sidebar.button("Reset"):
     placeholder = st.empty()
--- python_engine/src/core.py
+++ python_engine/src/core.py
@@ -1,17 +1,15 @@
+import torch
 from typing import List, Tuple
-from functools import lru_cache
-from .utils import get_transcript_yt, search_transcript, vtt_to_corpus
-from pyqna.models.reading_comprehension.transformer_models import TransformerQnAModel
+from .utils import get_transcript_yt, search_transcript, vtt_to_corpus, clean_text
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
+model = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
 
 
-model = TransformerQnAModel(
-    {"model_name": "distilbert-base-uncased-distilled-squad", "pre_trained": True}
-)
-
-
-@lru_cache(maxsize=100)
 def get_batched_corpus(vtt_path: str, batch_size: int) -> List[str]:
-    corpus = vtt_to_corpus(vtt_path)
+    corpus = clean_text(vtt_to_corpus(vtt_path))
     corpus_words = corpus.split()
     corpus_len = len(corpus_words)
     corpus_batches = [
@@ -21,14 +19,16 @@ def get_batched_corpus(vtt_path: str, batch_size: int) -> List[str]:
     return corpus_batches
 
 
-def get_answer_with_timestamp(url: str, question: str) -> Tuple[str]:
+def get_answer_with_timestamp(url: str, question: str) -> Tuple[str, int]:
     with open("subs.vtt", "w+") as subs:
         subs.write(get_transcript_yt(url)["subtitles"])
     corpus_batches = get_batched_corpus("subs.vtt", 512)
     answers = list()
     for batch in corpus_batches:
-        answers.append(model.get_answer(batch, question))
+        answers.append(question_answerer(question=question, context=batch))
+    print(answers)
     best_answer = sorted(answers, key=lambda d: d["score"], reverse=True)[0]["answer"]
-    print(best_answer)
+    print("best answer:", best_answer)
     best_answer_ts = search_transcript(best_answer, "subs.vtt")
-    return best_answer_ts[0], best_answer
+    print("best_answer_ts", best_answer_ts)
+    return best_answer, best_answer_ts[0]
