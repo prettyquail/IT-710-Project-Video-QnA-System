c98bf1b1e80ef262154c019b0586955761da1606
Saahil Ali <programmer290399@gmail.com>
2021-12-03 13:24:44 +0530

    the night gets darkest right before dawn

--- QnA_app/views.py
+++ QnA_app/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import *
 import json
-# from pyqna.models.reading_comprehension.transformer_models import TransformerQnAModel
-# from python_engine.src import (
-#     get_transcript_yt,
-#     vtt_to_corpus,
-#     search_transcript,
-#     get_video_id,
-# )
 
-#
-# model = TransformerQnAModel(
-#     {"model_name": "distilbert-base-uncased-distilled-squad", "pre_trained": True}
-# )
-#
-
+from python_engine.src import (
+    get_video_id,
+)
 
 
 def home(request):
@@ -21,7 +11,7 @@ def home(request):
     print("URL=", URL)
     answer = None
     question = request.POST.get("ques")
-    video_id = "HcqpanDadyQ"
+    video_id = "HcqpanDadyQ" if not URL else get_video_id(URL)
     timestamp, context = 0, {"turl": "https://www.youtube.com/embed/" + "HcqpanDadyQ"}
     if URL and question:
         # timestamp, answer = get_answer_with_timestamp(URL, question)
@@ -35,40 +25,19 @@ def home(request):
             + f"?start={timestamp}&autoplay=1"
         )
         context = {"answer": answer, "turl": Turl}
+    elif URL:
+
+        Turl = "https://www.youtube.com/embed/" + get_video_id(URL)
+        context = {"answer": answer, "turl": Turl}
+    elif question:
+        timestamp, answer = 32, "returned answer"
+        Turl = (
+            "https://www.youtube.com/embed/"
+            + "HcqpanDadyQ"
+            + f"?start={timestamp}&autoplay=1"
+        )
+        context = {"answer": answer, "turl": Turl}
+    else:
+        print("You're fucked!!")
 
     return render(request, "home.html", context)
-
-
-
-
-
-
-# Create your views here.
-# def home(request):
-#     URL = request.GET.get("URL")
-#     print("URL=", URL)
-#     answer="Machine learning is the study of computer algorithms that can improve automatically through experience and by the use of data. It is seen as a part of artificial intelligence."
-#     question = request.POST.get("ques")
-#     # if URL:
-#     #     with open("subs.vtt", "w+") as vtt_file:
-#     #         vtt_file.write(get_transcript_yt(URL)["subtitles"])
-#     #         corpus = vtt_to_corpus("subs.vtt")
-#     #
-#     #         answer = model.get_answer(corpus, question)
-#     #         print("*" * 32, answer, "*" * 32)
-#     print("Answer:",answer,"Question:",question)
-#     # video_id = get_video_id(URL)
-#     # url = "https://www.youtube.com/embed/" + (video_id if video_id else "HcqpanDadyQ")
-#     # print(url)
-#
-#     # timestamps = search_transcript(answer, "subs.vtt")
-#     # url += f"?t={timestamps[0]}"
-#
-#     context={}
-#     URL = "https://www.youtube.com/embed/HcqpanDadyQ"
-#     Turl = "https://www.youtube.com/embed/HcqpanDadyQ?start=45&autoplay=1"
-#     if URL and question:
-#         context = {"url":URL,"answer":answer,"turl":Turl}
-#     return render(request, "home.html", context)
-
-
---
+++ python_engine/src/core.py
@@ -0,0 +1,65 @@
+import torch
+from typing import List, Tuple
+from .utils import get_transcript_yt, search_transcript, vtt_to_corpus, clean_text
+
+# from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+
+# model_name = "lysandre/bidaf-elmo-model-2020.03.19"
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
+
+
+# def get_batched_corpus(vtt_path: str, batch_size: int) -> List[str]:
+#     corpus = clean_text(vtt_to_corpus(vtt_path))
+#     print(corpus)
+#     corpus_words = corpus.split()
+#     corpus_len = len(corpus_words)
+#     corpus_batches = [
+#         " ".join(corpus[idx : idx + batch_size])
+#         for idx in range(0, corpus_len, batch_size)
+#     ]
+#     return corpus_batches
+
+
+# def get_answer_with_timestamp(url: str, question: str) -> Tuple[str, int]:
+#     with open("subs.vtt", "w+") as subs:
+#         subs.write(get_transcript_yt(url)["subtitles"])
+#     corpus_batches = get_batched_corpus("subs.vtt", 512)
+#     answers = list()
+#     for batch in corpus_batches:
+#         answers.append(question_answerer(question=question, context=batch))
+#     print(answers)
+#     best_answer = sorted(answers, key=lambda d: d["score"], reverse=True)[0]["answer"]
+#     best_answer = " ".join([word.replace(" ", "") for word in best_answer.split("  ")])
+#     print("best answer:", best_answer)
+#     best_answer_ts = search_transcript(best_answer, "subs.vtt")
+#     print("best_answer_ts", best_answer_ts)
+#     return best_answer, best_answer_ts[0]
+
+
+import allennlp_models
+from allennlp.predictors.predictor import Predictor
+
+predictor = Predictor.from_path("hf://lysandre/bidaf-elmo-model-2020.03.19")
+
+
+def get_answer_with_timestamp_unbatched(url: str, question: str) -> Tuple[str, int]:
+    with open("subs.vtt", "w+") as subs:
+        subs.write(get_transcript_yt(url)["subtitles"])
+    corpus = vtt_to_corpus("subs.vtt")
+
+    print("===" * 32)
+    print(corpus)
+    print("===" * 32)
+    predictor_input = {
+        "passage": corpus,
+        "question": question,
+    }
+    predictions = predictor.predict_json(predictor_input)
+    start, end = predictions["best_span"]
+    answer = " ".join(predictions["passage_tokens"][start : end + 1])
+    print("Unbatched Answer:", answer)
+    best_answer_ts = search_transcript(answer, "subs.vtt")
+    print("Unbatched best_answer_ts", best_answer_ts)
+    return answer, best_answer_ts[0]
