56e692ccab81f528e9a271e05a5b2f4e51b693a2
Saahil Ali <programmer290399@gmail.com>
2021-12-03 22:08:55 +0530

    final demo ready push

--- python_engine/src/utils.py
+++ python_engine/src/utils.py
@@ -1,20 +1,10 @@
 import webvtt
+import difflib
 import requests
 import youtube_dl
+from functools import lru_cache
 from typing import List, Dict
-import nltk
-import re
-import string
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from urllib.parse import urlparse, parse_qs
-
-
-default_stemmer = PorterStemmer()
-default_stopwords = stopwords.words("english")  # or any other list of your choice
-
-import difflib
 
 
 def get_overlap(s1, s2):
@@ -23,35 +13,7 @@ def get_overlap(s1, s2):
     return size
 
 
-def clean_text(
-    text,
-):
-    def tokenize_text(text):
-        return [w for s in sent_tokenize(text) for w in word_tokenize(s)]
-
-    def remove_special_characters(text, characters=string.punctuation.replace("-", "")):
-        tokens = tokenize_text(text)
-        pattern = re.compile("[{}]".format(re.escape(characters)))
-        return " ".join(filter(None, [pattern.sub("", t) for t in tokens]))
-
-    def stem_text(text, stemmer=default_stemmer):
-        tokens = tokenize_text(text)
-        return " ".join([stemmer.stem(t) for t in tokens])
-
-    def remove_stopwords(text, stop_words=default_stopwords):
-        tokens = [w for w in tokenize_text(text) if w not in stop_words]
-        return " ".join(tokens)
-
-    text = text.strip(" ")  # strip whitespaces
-    text = text.lower()  # lowercase
-    # text = stem_text(text)  # stemming
-    # text = remove_special_characters(text)  # remove punctuation and symbols
-    # text = remove_stopwords(text)  # remove stopwords
-    # text.strip(" ")  # strip whitespaces again
-
-    return text
-
-
+@lru_cache(maxsize=100)
 def get_transcript_yt(url: str) -> Dict:
     """A simple function to get transcript of a YouTube Video
 
