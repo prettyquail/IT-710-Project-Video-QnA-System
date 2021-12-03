6c5eca7bd7e18d5070b89ae8c3c2d1300a93eb30
unknown <manishasahu1903@gmail.com>
2021-12-03 12:51:58 +0530

    Small change

--- QnA_app/views.py
+++ QnA_app/views.py
@@ -16,31 +16,59 @@ import json
 
 
 
-
-# Create your views here.
 def home(request):
     URL = request.GET.get("URL")
     print("URL=", URL)
-    answer="Machine learning is the study of computer algorithms that can improve automatically through experience and by the use of data. It is seen as a part of artificial intelligence."
+    answer = None
     question = request.POST.get("ques")
-    # if URL:
-    #     with open("subs.vtt", "w+") as vtt_file:
-    #         vtt_file.write(get_transcript_yt(URL)["subtitles"])
-    #         corpus = vtt_to_corpus("subs.vtt")
-    #
-    #         answer = model.get_answer(corpus, question)
-    #         print("*" * 32, answer, "*" * 32)
-    print("Answer:",answer,"Question:",question)
-    # video_id = get_video_id(URL)
-    # url = "https://www.youtube.com/embed/" + (video_id if video_id else "HcqpanDadyQ")
-    # print(url)
-
-    # timestamps = search_transcript(answer, "subs.vtt")
-    # url += f"?t={timestamps[0]}"
-
-    context={}
-    URL = "https://www.youtube.com/embed/HcqpanDadyQ"
-    Turl = "https://www.youtube.com/embed/HcqpanDadyQ?start=45&autoplay=1"
+    video_id = "HcqpanDadyQ"
+    timestamp, context = 0, {"turl": "https://www.youtube.com/embed/" + "HcqpanDadyQ"}
     if URL and question:
-        context = {"url":URL,"answer":answer,"turl":Turl}
+        # timestamp, answer = get_answer_with_timestamp(URL, question)
+        timestamp, answer = 32, "returned answer"
+        print("*" * 32, answer, "*" * 32)
+        print("Answer:", answer, "Question:", question)
+
+        Turl = (
+            "https://www.youtube.com/embed/"
+            + video_id
+            + f"?start={timestamp}&autoplay=1"
+        )
+        context = {"answer": answer, "turl": Turl}
+
     return render(request, "home.html", context)
+
+
+
+
+
+
+# Create your views here.
+# def home(request):
+#     URL = request.GET.get("URL")
+#     print("URL=", URL)
+#     answer="Machine learning is the study of computer algorithms that can improve automatically through experience and by the use of data. It is seen as a part of artificial intelligence."
+#     question = request.POST.get("ques")
+#     # if URL:
+#     #     with open("subs.vtt", "w+") as vtt_file:
+#     #         vtt_file.write(get_transcript_yt(URL)["subtitles"])
+#     #         corpus = vtt_to_corpus("subs.vtt")
+#     #
+#     #         answer = model.get_answer(corpus, question)
+#     #         print("*" * 32, answer, "*" * 32)
+#     print("Answer:",answer,"Question:",question)
+#     # video_id = get_video_id(URL)
+#     # url = "https://www.youtube.com/embed/" + (video_id if video_id else "HcqpanDadyQ")
+#     # print(url)
+#
+#     # timestamps = search_transcript(answer, "subs.vtt")
+#     # url += f"?t={timestamps[0]}"
+#
+#     context={}
+#     URL = "https://www.youtube.com/embed/HcqpanDadyQ"
+#     Turl = "https://www.youtube.com/embed/HcqpanDadyQ?start=45&autoplay=1"
+#     if URL and question:
+#         context = {"url":URL,"answer":answer,"turl":Turl}
+#     return render(request, "home.html", context)
+
+
